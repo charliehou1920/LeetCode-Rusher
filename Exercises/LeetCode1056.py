ae6aa38a66379ec086c726be1945e067f990dfd9
NorthSeaPark <charliehou308@gmail.com>
2023-06-05 16:16:26 -0400

    go

--- Exercises/LeetCode1056.py
+++ Exercises/LeetCode1056.py
@@ -1,4 +1,4 @@
-def confusingNumber(self, n: int) -> bool:
+def confusingNumber(n):
     valid_digits = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
     num_str = str(n)
     reverse_num = ''
@@ -11,5 +11,14 @@ def confusingNumber(self, n: int) -> bool:
         print('reverse_num:', reverse_num)
 
     return reverse_num != num_str
+
+def main():
+    n = 9
+    if(confusingNumber(n)):
+        print("It is a Confusing Number!")
+    else:
+        print("It is not a Confusing Number!")
         
 
+if __name__ == "__main__":
+    main()
--- Exercises/LeetCode280.py
+++ Exercises/LeetCode280.py
@@ -1,10 +1,17 @@
 '''
 给你一个的整数数组 nums, 将该数组重新排序后使 nums[0] <= nums[1] >= nums[2] <= nums[3]... 
 '''
-def wiggleSort(self, nums: List[int]) -> None:
+def wiggleSort(nums):
     nlen = len(nums)
     nums.sort()
     for i in range(nlen - 1):
         if i % 2:
             nums[i], nums[i+1] = nums[i+1], nums[i]
 
+def main():
+    nums =  [3,5,2,1,6,4]
+    wiggleSort(nums)
+    print(nums)
+
+if __name__ == "__main__":
+    main()
